36854d69265d71f546cb4a9b36cf9e2cc2e03bec
Ben Peterson <benponline@outlook.com>
2021-03-09 13:47:23 -0500

    Update Get-SubRedditTopImage.ps1

--- Get-SubRedditTopImage.ps1
+++ Get-SubRedditTopImage.ps1
@@ -4,13 +4,18 @@ param (
     [string]$Path = "."   
 )
 
-$uri = "www.reddit.com/r/$SubReddit/top"
-$subredditTopPage = Invoke-WebRequest -Uri $uri
+$subredditTopUri = "www.reddit.com/r/$SubReddit/top"
+$subredditTopPage = Invoke-WebRequest -Uri $subredditTopUri
+
+# This line extracts the links out of the web request to the subreddit top page. The first link that contains "/comments/" in the URI is tipically the URI that points to the post.
 $postWebLink = $subredditTopPage.links | Where-Object -Property href -Match "/comments/" | Select-Object -First 1
 
-# Grab image from post
 $dateStamp =  Get-Date -UFormat %Y-%m-%d 
+
 $postWebPage = Invoke-WebRequest -Uri $postWebLink.href
+
+# This line extracts the links out of the web request to the post page. This URI that contains the link to the image contains "i.redd.it".
 $postImageLink = $postWebPage.links | Where-Object -Property href -match "i.redd.it" | Select-Object -First 1 
+
+# Download the image to the directory the script is in or the directory given by the user.
 Invoke-WebRequest -Uri $postImageLink.href -OutFile "$Path\$dateStamp $SubReddit.jpg"
-#Invoke-WebRequest -Uri https://i.redd.it/p7dzgl3iptl61.png -OutFile "C:\PowerShellOutput\$dateStamp $subreddit.jpg"
