4c96d7a4b785a7af78853e63032510a5bd019bc0
Ben Peterson <benponline@outlook.com>
2021-03-12 08:11:58 -0500

    Update parse-html.ps1

--- parse-html.ps1
+++ parse-html.ps1
@@ -1,8 +1,34 @@
-$pageHtml = (Invoke-WebRequest -Uri "www.reddit.com/r/wallpapers/top").RawContent
+$pageRequest = Invoke-WebRequest -Uri "www.reddit.com/r/wallpapers/top"
+$pageHtml = $pageRequest.RawContent.ToCharArray()
 
-$open = "<"
-$tag = "a"
+#$pageHtml = "123456789 HTML 12345676".ToCharArray()
 
-foreach($char in $pageHtml){
+$opener = "html"
+$opener = $opener.ToCharArray()
+$closer = "</a>"
+$links = @()
 
+for ($i = 0; $i -lt $pageHtml.Count; $i++) {
+    $link = ""
+
+    # Check for opener
+    $openerStart = $true
+    for ($O = 0; $O -lt $opener.Count; $O++) {
+        #$opener[$O]
+        if ($pageHtml[$i + $O] -ne $opener[$O]) {
+            $openerStart = $false
+            $link = ""
+            break
+        }
+
+        $link += $pageHtml[$i]
+    }
+
+    if($openerStart -eq $true){
+        Write-Host "Found Starter." 
+        
+        #Record charactors untill closer found.
+    }
 }
+
+$links
