1ac451cf1f69aa28502dc75fc6751c6f5cd430b9
Ben Peterson <benponline@outlook.com>
2021-03-13 13:11:18 -0500

    First script works randomly
    Not sure why it fails randomly.

--- splitString.ps1
+++
@@ -1,8 +0,0 @@
-$a = 'https://www.reddit.com/r/wallpapers/comments/2z4wm/towards_the_sun_2560x1440/","events":[],"ev'
-
-$b = ($a -split "(/)", 9)[0..15] -join ""
-#$b = $b[0..15] -join ""
-$b
-
-#$b = ((($a -split "(/)", -4)[0]) + "/")
-#$b
