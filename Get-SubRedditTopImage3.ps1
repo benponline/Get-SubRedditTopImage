36af9a54de05848b1dbbcaf7d482f01262a678c8
Ben Peterson <benponline@outlook.com>
2021-03-14 23:14:17 -0400

    fixing scrapping

--- Get-SubRedditTopImage3.ps1
+++ Get-SubRedditTopImage3.ps1
@@ -10,7 +10,8 @@ $subredditTopPage = Invoke-WebRequest -Uri $subredditTopUri
 #Write-Verbose ($subredditTopPage.headers)
 
 # This line extracts the links out of the web request to the subreddit top page. The first link that contains "/comments/" in the URI is tipically the URI that points to the post.
-$postWebLink = $subredditTopPage.links | Where-Object -Property href -Match "/comments/" | Select-Object -First 1
+$postWebLink = $subredditTopPage.links | Where-Object -Property href -NotMatch "discord" | Where-Object -Property href -Match "/comments/" | Select-Object -First 1
+#$postWebLink.href
 
 $dateStamp =  Get-Date -UFormat %Y-%m-%d 
 
@@ -20,9 +21,11 @@ $postWebPage = Invoke-WebRequest -Uri ($postWebLink.href)
 
 # This line extracts the links out of the web request to the post page. This URI that contains the link to the image contains "i.redd.it".
 $postImageLink = $postWebPage.links | Where-Object -Property href -match "i.redd.it" | Select-Object -First 1
+$postImageLink
 
 if($postImageLink.Count -lt 1){
     $postImageLink = $postWebPage.links | Where-Object -Property href -match "i.imgur.com" | Select-Object -First 1
+    $postImageLink
 }
 
 Write-Verbose ("Image URI: " + $postImageLink.href)
