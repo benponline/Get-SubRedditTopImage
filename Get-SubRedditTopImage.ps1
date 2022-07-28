0c0ee5944cf048a264d12b91b77ec5dd7754c7e2
Ben Peterson <benponline@outlook.com>
2022-07-28 09:52:04 -0800

    Update URI to post
    Reddit changed how they address handle links to posts.

--- Get-SubRedditTopImage.ps1
+++ Get-SubRedditTopImage.ps1
@@ -13,27 +13,21 @@ $subredditTopPage = Invoke-WebRequest -Uri $subredditTopUri
 # This line extracts the links out of the web request to the subreddit top page.
 # The first link that contains "/comments/" in the URI is tipically the URI that points to the post.
 # Links with the string "discord" are filtered out to prevent getting links to Discord channels.
-$postWebLink = $subredditTopPage.links | 
-    Where-Object -Property href -NotMatch "discord" | 
-    Where-Object -Property href -Match "/comments/" | 
-    Select-Object -First 1
+$postWebLink = $subredditTopPage.links | Where-Object -Property href -NotMatch "discord" | Where-Object -Property href -Match "/comments/" | Select-Object -First 1
 
-if($postWebLink.href -lt 1){
+if($null -eq $postWebLink.href){
     Write-Host "Unable to get image from the $SubReddit subreddit"
     return
 } 
 
-Write-Verbose ("Requesting webpage for " + $postWebLink.href + ".")
-$postWebPage = Invoke-WebRequest -Uri ($postWebLink.href)
+$postWebPage = Invoke-WebRequest -Uri ("https://www.reddit.com" + $postWebLink.href)
 
 # This line extracts the links out of the web request to the post page. This URI that contains the link to the image contains "i.redd.it".
 $postImageLink = $postWebPage.links | Where-Object -Property href -match "i.redd.it" | Select-Object -First 1
 
-if($postImageLink.Count -lt 1){
+if($null -eq $postImageLink){
     $postImageLink = $postWebPage.links | Where-Object -Property href -match "i.imgur.com" | Select-Object -First 1
 }
-
-Write-Verbose ("Requesting image from " + $postImageLink.href)
 
 if($null -eq $postImageLink.href){
     Write-Host "Unable to get a top image post from the $SubReddit subreddit."
